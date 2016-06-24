678ab44a1303338608d86dc973c7f2fc848076b7
jackieLin <dashi_lin@163.com>
2016-06-24 15:58:34 +0800

    finish: 完成首页的切图

--- index.ios.js
+++ index.ios.js
@@ -7,12 +7,14 @@ import React, { Component } from 'react';
 import { Provider} from 'react-redux';
 import configureStore from './store/configureStore';
 import App from './compoents/app';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import {
   AppRegistry,
   StyleSheet,
   Text,
   Image,
+  View,
   Navigator,
   TouchableOpacity
 } from 'react-native';
@@ -41,10 +43,26 @@ var styles = StyleSheet.create({
     height: null
   },
 
-  navigator: {
+  navigatorLeft: {
     transform: [
       {translateX: 230}
     ]
+  },
+
+  navigatorRight: {
+    transform: [
+      {translateX: -110}
+    ]
+  },
+
+  shadowLeft: {
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    shadowOffset: {width: -5, height: 0}
+  },
+
+  shadowRight: {
+    shadowOffset: {width: 5, height: 0}
   }
 });
 
@@ -97,18 +115,67 @@ class v2ex extends Component {
       <Provider store={store}>
         <Image
           source={require('./images/bg.jpg')}
-          style={styles.bg}>
-              <Navigator
-                debugOverlay={false}
-                initialRoute={{ route: '/', title: 'V2EX' }}
-                renderScene={this.renderScene}
-                navigationBar={
-                  <Navigator.NavigationBar
-                    routeMapper={NavigationBarRouteMapper}
-                    style={{backgroundColor: 'white', flexDirection: 'row'}}
+          style={[styles.bg, {width: 375}]}>
+              <View style={{position: 'absolute', top: 0, left: 0, width: 230, backgroundColor: 'transparent'}}>
+                <View style={{height: 180, flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+                    <Image
+                      source={{uri: 'https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png'}}
+                      style={{width: 75, height: 75, borderWidth: 2, borderColor: '#f5f5ea', borderRadius: 38, }}
+                      resizeMode={'cover'}
+                    />
+                  <Text style={{marginTop: 13, fontSize: 15, color: '#353535'}}>jackieLin</Text>
+                </View>
+
+                <View>
+                    <View style={{height: 55, backgroundColor: 'rgba(255, 255, 255, .3)', flexDirection: 'row', alignItems: 'center', marginTop: 1}}>
+                      <Icon name="user" size={20} style={{marginLeft: 22, color: '#49545d', marginRight: 22}}/>    
+                      <Text style={{color: '#49545d', fontSize: 15}}>个人中心</Text>
+                    </View>
+                    <View style={{height: 55, backgroundColor: 'rgba(255, 255, 255, .3)', flexDirection: 'row', alignItems: 'center', marginTop: 1}}>
+                      <Icon name="bell-o" size={20} style={{marginLeft: 22, color: '#49545d', marginRight: 22}}/>    
+                      <Text style={{color: '#49545d', fontSize: 15}}>消息提醒</Text>
+                    </View>
+                    <View style={{height: 55, backgroundColor: 'rgba(255, 255, 255, .3)', flexDirection: 'row', alignItems: 'center', marginTop: 1}}>
+                      <Icon name="bell-o" size={20} style={{marginLeft: 22, color: '#49545d', marginRight: 22}}/>    
+                      <Text style={{color: '#49545d', fontSize: 15}}>我的收藏</Text>
+                    </View>
+                    <View style={{height: 55, backgroundColor: 'rgba(255, 255, 255, .3)', flexDirection: 'row', alignItems: 'center', marginTop: 10}}>
+                      <Icon name="location-arrow" size={20} style={{marginLeft: 22, color: '#49545d', marginRight: 22}}/>    
+                      <Text style={{color: '#49545d', fontSize: 15}}>节点</Text>
+                    </View>
+
+                    <View style={{height: 55, backgroundColor: 'rgba(255, 255, 255, .3)', flexDirection: 'row', alignItems: 'center', marginTop: 1}}>
+                      <Icon name="info-circle" size={20} style={{marginLeft: 22, color: '#49545d', marginRight: 22}}/>    
+                      <Text style={{color: '#49545d', fontSize: 15}}>更多</Text>
+                    </View>
+                </View>
+              </View>
+
+              <View style={{position: 'absolute', top: 0, right: 0, width: 110, backgroundColor: 'transparent', marginTop: 32}}>
+                <View style={{height: 55, marginTop: 1, backgroundColor: 'rgba(255, 255, 255, .4)', paddingRight: 25}}>
+                    <Text style={{textAlign: 'right', lineHeight: 38, fontSize: 15, color: '#3c4d57'}}>Technology</Text>
+                </View>
+                <View style={{height: 55, marginTop: 1, backgroundColor: 'rgba(255, 255, 255, .4)', paddingRight: 25}}>
+                    <Text style={{textAlign: 'right', lineHeight: 38, fontSize: 15, color: '#3c4d57'}}>Creative</Text>
+                </View>
+                <View style={{height: 55, marginTop: 1, backgroundColor: 'rgba(255, 255, 255, .4)', paddingRight: 25}}>
+                    <Text style={{textAlign: 'right', lineHeight: 38, fontSize: 15, color: '#3c4d57'}}>Play</Text>
+                </View>
+              </View>
+              <View 
+                style={[styles.container, styles.navigatorLeft, styles.shadowLeft]}>
+                  <Navigator
+                    debugOverlay={false}
+                    initialRoute={{ route: '/', title: 'V2EX' }}
+                    renderScene={this.renderScene}
+                    navigationBar={
+                      <Navigator.NavigationBar
+                        routeMapper={NavigationBarRouteMapper}
+                        style={{backgroundColor: 'white', flexDirection: 'row'}}
+                      />
+                    }
                   />
-                }
-              />
+              </View>
         </Image>
       </Provider>
     );
