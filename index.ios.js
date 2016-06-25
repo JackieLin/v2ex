865006c2eed3163b6d59fffd63b53b9fad1bf6a3
jackieLin <dashi_lin@163.com>
2016-06-25 19:49:09 +0800

    finish 完成首页动画效果

--- index.ios.js
+++ index.ios.js
@@ -16,7 +16,8 @@ import {
   Image,
   View,
   Navigator,
-  TouchableOpacity
+  TouchableOpacity,
+  Animated
 } from 'react-native';
 
 // 内嵌 redux
@@ -62,41 +63,20 @@ var styles = StyleSheet.create({
   },
 
   shadowRight: {
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
     shadowOffset: {width: 5, height: 0}
   }
 });
 
-var NavigationBarRouteMapper = {
-  LeftButton: function(route, navigator, index, navState) {
-    return (
-      <TouchableOpacity
-        onPress={() => navigator.pop()}
-        style={{width: 100, height: 60}}>
-        <Image source={require('./images/menu.png')} style={{position: 'absolute', top: 15, left: 10, height: 18, width: 27}}/>
-      </TouchableOpacity>
-    );
-  },
-
-  RightButton: function(route, navigator, index, navState) {
-    return (
-      <TouchableOpacity
-        onPress={() => navigator.push(newRandomRoute())}
-        style={{width: 100, height: 60}}>
-        <Image source={require('./images/more.png')} style={{position: 'absolute', top: 15, right: 10, height: 18, width: 27}}/>
-      </TouchableOpacity>
-    );
-  },
-
-  Title: function(router, navigator, index, navState) {
-    return (
-      <Text style={{lineHeight: 32, fontSize: 15}}>
-        {router.title}
-      </Text>
-    );
-  },
-};
-
 class v2ex extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nav: new Animated.Value(0),
+      shadow: styles.shadowLeft
+    };
+  }
   /**
    * 路由跳转
    */
@@ -109,11 +89,77 @@ class v2ex extends Component {
     }
   }
 
+  navAnimate(toValue) {
+    let isToggle = false;
+
+    return (style) => {
+      let value = isToggle ? 0 : toValue;
+      
+      Animated.timing(
+         this.state.nav,
+         {toValue: value},
+       ).start();
+
+      isToggle = !isToggle;
+    };
+  }
+
+  /**
+   * 显示左边的 view, 使用动画效果
+   */
+  toggleLeftNav() {
+    return this.navAnimate(230);
+  }
+  
+  /**
+   * 显示右边的 view, 使用动画效果
+   */
+  toggleRightNav() {
+    return this.navAnimate(-110);
+  }
+
+  routeMapper() {
+    var self = this;
+    var navigationBarRouteMapper = {
+        LeftButton: function(route, navigator, index, navState) {
+          let leftNav = self.toggleLeftNav();
+          return (
+            <TouchableOpacity
+              onPress={leftNav.bind(self, 'shadowLeft')}
+              style={{width: 100, height: 60}}>
+              <Image source={require('./images/menu.png')} style={{position: 'absolute', top: 15, left: 10, height: 18, width: 27}}/>
+            </TouchableOpacity>
+          );
+        },
+
+        RightButton: function(route, navigator, index, navState) {
+          let rightNav = self.toggleRightNav();
+          return (
+            <TouchableOpacity
+              onPress={rightNav.bind(self, 'shadowRight')}
+              style={{width: 100, height: 60}}>
+              <Image source={require('./images/more.png')} style={{position: 'absolute', top: 15, right: 10, height: 18, width: 27}}/>
+            </TouchableOpacity>
+          );
+        },
+
+        Title: function(router, navigator, index, navState) {
+          return (
+            <Text style={{lineHeight: 32, fontSize: 15}}>
+              {router.title}
+            </Text>
+          );
+        },
+      };
+
+      return navigationBarRouteMapper;
+  }
+
   render() {
     // console.warn(JSON.stringify(store.getState()));
     return (
       <Provider store={store}>
-        <Image
+          <Image
           source={require('./images/bg.jpg')}
           style={[styles.bg, {width: 375}]}>
               <View style={{position: 'absolute', top: 0, left: 0, width: 230, backgroundColor: 'transparent'}}>
@@ -162,20 +208,20 @@ class v2ex extends Component {
                     <Text style={{textAlign: 'right', lineHeight: 38, fontSize: 15, color: '#3c4d57'}}>Play</Text>
                 </View>
               </View>
-              <View 
-                style={[styles.container, styles.navigatorLeft, styles.shadowLeft]}>
+              <Animated.View 
+                style={[styles.container, {transform: [{translateX: this.state.nav}]}, this.state.shadow]}>
                   <Navigator
                     debugOverlay={false}
                     initialRoute={{ route: '/', title: 'V2EX' }}
                     renderScene={this.renderScene}
                     navigationBar={
                       <Navigator.NavigationBar
-                        routeMapper={NavigationBarRouteMapper}
+                        routeMapper={this.routeMapper()}
                         style={{backgroundColor: 'white', flexDirection: 'row'}}
                       />
                     }
                   />
-              </View>
+              </Animated.View>
         </Image>
       </Provider>
     );
